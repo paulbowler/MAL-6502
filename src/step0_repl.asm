72409b94c532069d5bfc89dc7e3920f356d59589
Paul Bowler <spbowler@gmail.com>
2023-02-18 16:22:57 +0000

    Added more intuitive REPL structure

--- src/step0_repl.asm
+++ src/step0_repl.asm
@@ -1,17 +1,28 @@
 osword = &FFF1
 oswrch = &FFEE
+osbyte = &FFF4
 
-ORG &2000         ; code origin
+ORG &2000
 
 .start
     LDX #0
 .prompt
     LDA prompt_text, X
     CMP #0
-    BEQ read
+    BEQ loop
     JSR oswrch
     INX
     JMP prompt
+.loop
+    JSR read
+    BCS exit    ; Exit if escape pressed
+
+    CPY #0      ; If no input do nothing
+    BEQ loop
+
+    JSR eval
+    JSR print
+    JMP loop
 .read
     LDA #ASC("]")
     JSR oswrch
@@ -19,22 +30,28 @@ ORG &2000         ; code origin
     LDX #readline MOD 256
     LDY #readline DIV 256
     JSR osword
-    BCS exit
+    RTS
 .eval
     LDX #0
+    RTS
 .print
     CPY #0
-    BEQ loop
+    BEQ done
     LDA buffer,X
     JSR oswrch
     INX
     DEY
     JMP print
-.loop
+    RTS
+.done
     LDA #10:JSR oswrch
     LDA #13:JSR oswrch
-    JMP read
+    RTS
 .exit
+    LDA #126            ; Acknowledge escape key pressed (*FX 126)
+    JSR osbyte
+    LDA #10:JSR oswrch  ; Print new line to
+    LDA #13:JSR oswrch  ; exit gracefully
     RTS
 
 .readline
