9c828a3ba45d909b85d8b857fa8796b3dd9818e9
Paul Bowler <spbowler@gmail.com>
2023-02-18 15:45:58 +0000

    Implemented read/print
    Simply echo user input

--- src/step0_repl.asm
+++ src/step0_repl.asm
@@ -1,25 +1,52 @@
+osword = &FFF1
 oswrch = &FFEE
-osasci = &FFE3
-addr = &70
 
-ORG &2000         ; code origin (like P%=&2000)
+ORG &2000         ; code origin
 
 .start
-    LDA #22:JSR oswrch
-    LDA #7:JSR oswrch
-    LDA #mytext MOD 256:STA addr
-    LDA #mytext DIV 256:STA addr+1
-    LDY #0
+    LDX #0
+.prompt
+    LDA prompt_text, X
+    CMP #0
+    BEQ read
+    JSR oswrch
+    INX
+    JMP prompt
+.read
+    LDA #ASC("]")
+    JSR oswrch
+    LDA #0
+    LDX #readline MOD 256
+    LDY #readline DIV 256
+    JSR osword
+    BCS exit
+.eval
+    LDX #0
+.print
+    CPY #0
+    BEQ loop
+    LDA buffer,X
+    JSR oswrch
+    INX
+    DEY
+    JMP print
 .loop
-    LDA (addr),Y
-    BEQ finished
-    JSR osasci
-    INY
-    BNE loop
-.finished
+    LDA #10:JSR oswrch
+    LDA #13:JSR oswrch
+    JMP read
+.exit
     RTS
 
-.mytext EQUS "Hello world!", 13, 0
+.readline
+    EQUW buffer
+    EQUB &FF
+    EQUB 32
+    EQUB 127
+.prompt_text
+    EQUS "MAL v1.0"
+    EQUB 10:EQUB 13
+.buffer
+    EQUB 0
 .end
 
 SAVE "mal6502", start, end 
